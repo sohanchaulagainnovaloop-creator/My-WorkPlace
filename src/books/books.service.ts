e5be3f39a008be7214dbd3551b8149de30d539a7
sohanchaulagainnovaloop-creator <sohanchaulagainnovaloop@gmail.com>
2026-08-06 15:35:29 +0545

    initial commit

---
+++ src/books/books.service.ts
@@ -0,0 +1,29 @@
+﻿import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BooksService {
+  private books = [
+    {
+      id: 1,
+      title: 'Atomic Habits',
+      author: 'James Clear',
+      price: 500,
+    },
+    {
+      id: 2,
+      title: 'Rich Dad Poor Dad',
+      author: 'Robert Kiyosaki',
+      price: 650,
+    },
+    {
+      id: 3,
+      title: 'Deep Work',
+      author: 'Cal Newport',
+      price: 700,
+    },
+  ];
+
+  findAll() {
+    return this.books;
+  }
+}
